5824b927dfd1d82ddf66f8d97bfcc9b24733a3d7
Ramesh K Munjal <rameshkmunjal@gmail.com>
2020-08-16 16:20:05 +0530

    page layout changed

--- projectDetails.js
+++ projectDetails.js
@@ -1,59 +1,69 @@
 let project =[
 	{
+		"type":"vanila",
 		"heading" :"Pokemon Game",
 		"details":"This Project is about accessing JSON data with use of Javascript .",
 		"technologies":"Techonologies - HTML, CSS, JAVASCRIPT    ",
 		"fileSrc":"Assignment1/index.html"	
 	},
 	{
+		"type":"vanila",
 		"heading" :"Big Bang Theory",
 		"details":"This Project is again about accessing JSON data with use of Javascript ES-6 and getting details of famous TV series Big Bang Theory .",
 		"technologies":"Techonologies - HTML, CSS, JAVASCRIPT    ",
 		"fileSrc":"Assignment2/problem2/bigbang.html"	
 	},
 	{
+		"type":"vanila",
 		"heading" :"My Portfolio",
 		"details":"This Project is about Responsive Web Design without Bootstrap, creating my own portfolio .", 
 		"technologies":"Techonologies - HTML , CSS, Javascript .  ",
 		"fileSrc":"Assignment3/portfolio.html"	
 	},
 	{
+		"type":"vanila",
 		"heading" :"WhatsApp Chat App",
 		"details":"This Project is about Responsive Web Design using Bootstrap, mimicking Whatsapp website .",
 		"technologies": "Techonologies - HTML , CSS, Bootstrap, Javascript .  ",
 		"fileSrc":"Assignment4/index.html"	
 	},
 	{
+		"type":"jquery",
 		"heading" :"Collapsible Accordion",
 		"details":"This Project is about creating Collapsible Accordion using Jquery and without help from Bootstrap or any UI .",
 		"technologies":"Techonologies - HTML, CSS, Jquery.   ",
 		"fileSrc":"Assignment5/Ques-1/index.html"	
 	},
 	{
+		"type":"jquery",
 		"heading" :"Navbar",
 		"details":"This Project is about creating Navbar using Jquery and without help from Bootstrap or any UI .",
 		"technologies": "Techonologies - HTML, CSS,  Jquery.   ",
 		"fileSrc":"Assignment5/Ques-2/index.html"	
 	},
 	{
+		"type":"jquery",
 		"heading" :"Modal",
 		"details":"This Project is about creating Modal using Jquery and without help from Bootstrap or any UI .",
 		"technologies":"Techonologies - HTML, CSS,  Jquery.   ",
 		"fileSrc":"Assignment5/Ques-3/index.html"	
 	},	
 	{
+		"type":"jquery",
 		"heading" :"IMDB Website",
 		"details":"This Project is about accessing imdb api with use of AJAX Jquery call  .",
 		"technologies": "Techonologies - HTML, CSS, Bootstrap, Jquery ",
 		"fileSrc":"Project2Assignment6/index.html"	
 	},
 	{
+		"type":"angular",
 		"heading" :"Game of Thrones",
 		"details":"This Project is about accessing Ice & Fire api using Angular.",
 		"technologies": "Techonologies - HTML, CSS, Bootstrap, Angular ",
 		"fileSrc":"iceNfire/index.html"	
 	},	
 	{
+		"type":"angular",
 		"heading" :"IMDB-Angular",
 		"details":"This Project is about accessing imdb api with use of Angular  .",
 		"technologies": "Techonologies - HTML, CSS, Bootstrap, Angular ",
--- projects.js
+++ projects.js
@@ -1,40 +1,79 @@
 
-let getId=(element)=>{
-	return document.getElementById(element);	
-}
-let $=(element)=>{
-	return document.createElement(element);	
-}
-let addElementsToDisplayArea=(row, p )=>{	
-	let col = $("div");
-	col.setAttribute("class" , "col-xs-12 col-sm-6 col-md-3 col-lg-3");	
-	let details=$("div");
-	details.setAttribute("class", "details");
-	details.innerHTML='<h3>'+p.heading+'</h6><p>'+p.details+'</p><p>'+p.technologies+'</p>';
-	let btnHolder=$("div");
-	btnHolder.setAttribute("class", "btnHolder");
-	btnHolder.innerHTML='<button class="btn"><a  href="'+p.fileSrc+'">View Page</a></button>';
-	col.appendChild(details);
-	col.appendChild(btnHolder);
-	row.appendChild(col);		
-	//console.log(displayArea);	
-}
-
-let init=()=>{
-	let displayArea = getId("displayArea");
-	let container = $("div");
-	container.setAttribute("class" , "container");
-	let row = $("div");
-	row.setAttribute("class" , "row");
-	let arrayLength = project.length;	
-	
-	for(let i=arrayLength - 1 ; i >= 0; i--){
-		let p = project[i];	
-		console.log(i);
-		addElementsToDisplayArea(row, p );		
+let populateDivWithData=(d, a)=>{
+	for(let i=0; i<a.length; i++){
+		let div =document.createElement('div');
+		div.setAttribute('class', 'box');
+		let h3=document.createElement('h3');
+		let h3Txt=document.createTextNode(a[i].heading);
+		h3.append(h3Txt);
+		let p=document.createElement('p');
+		let pTxt=document.createTextNode(a[i].details);
+		p.append(pTxt);
+		let aTag=document.createElement('a');
+		let aTxt=document.createTextNode('View Page');
+		aTag.setAttribute('href', a[i].fileSrc);
+		aTag.setAttribute('class', 'link');
+		aTag.append(aTxt);
+		p.append(aTag);
+		div.append(h3);
+		div.append(p);
+		d.append(div);		
 	}
-	container.appendChild(row);
-	displayArea.appendChild(container);
-
+	return d;
 }
+
+let getTechSection=(name)=>{
+	let div = document.createElement('div');
+	div.setAttribute('id', name);	
+	let a=project.filter((b)=>{
+		return b.type===name;
+	})
+	console.log(a);
+	let h2=document.createElement('h2');
+	let h2Txt=document.createTextNode(name);
+	h2.append(h2Txt);
+	div.append(h2);
+	div=populateDivWithData(div, a);	
+	return div;
+}
+let getDistinctValues=(arr)=>{
+	let newArr=[];
+	for(let i=0; i<arr.length; i++){
+		if(newArr.indexOf(arr[i]) === -1){
+			newArr.push(arr[i]);
+		}
+	}
+	return newArr;
+}
+
+let sortProjectHeadings=(items)=>{
+	let arr=[];
+	for(let item of items){
+		arr.push(item.type);
+	}
+	return arr;
+}
+
+//controller function 
+let init=()=>{
+	console.log(project);
+	let displayArea=document.getElementById('displayArea');
+	let projectArr=sortProjectHeadings(project);
+	console.log(projectArr);
+	projectArr=getDistinctValues(projectArr);
+	console.log(projectArr);
+	for(let i=0; i < projectArr.length; i++){
+		console.log(i);
+		let section=getTechSection(projectArr[i]);
+		displayArea.append(section);
+	}
+	console.log(displayArea);		
+}
+
+
+//as and when document loads - event handler
 window.onload=init;
+
+/*
+	
+	*/
