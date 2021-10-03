6d2db703c0a99a8a528b6a7dd070a30a6065ac35
ramesh kumar munjal <rameshkmunjal.com>
2021-10-03 19:27:46 +0530

    to do list project added

--- data/jsData.js
+++ data/jsData.js
@@ -1,18 +1,11 @@
 let jsData = [
     {
-        "id": "1",
-        "category": "web-design",
-        "name": "sign-up",
-        "created_on":"2021-08-17",
-        "url": "./projects/jsProjects/1_sign-up-page/index.html",
+        "id": "5",
+        "category": "vanillajs",
+        "name": "to-do-list",
+        "created_on":"2021-09-30",
+        "url": "./projects/jsProjects/5-todolist/index.html",
         "tech":"HTML CSS"
     },
-    {
-        "id": "2",
-        "category": "web-design",
-        "name": "checkout",
-        "created_on":"2021-08-16",
-        "url": "./projects/jsProjects/2_checkout/index.html",
-        "tech":"HTML CSS"
-    },
+    
 ];
