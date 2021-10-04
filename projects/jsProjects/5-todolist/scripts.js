c53880e4b3c6e03cad589010c7581ecc8b4ee7a3
ramesh kumar munjal <rameshkmunjal.com>
2021-10-04 21:43:20 +0530

    to do list code changes

--- projects/jsProjects/5-todolist/scripts.js
+++ projects/jsProjects/5-todolist/scripts.js
@@ -1,4 +1,4 @@
-/* utility function */
+/*--------------------- utility function--------------------------*/
 let $CE=(ele)=>document.createElement(ele);
 let $GID=(ele)=>{
     console.log(ele);
@@ -13,142 +13,35 @@ let $APCN=(parent, ...arr)=>{
     }
     return parent;
 }
-/*------------------create form related functions------------------------------------*/
-
-//event listener to accept input and call functions to update data and add table row
-//function to get id of form and adding submit event listener
-let saveBtnHandler=(saveBtn, allTasks)=>{
-    saveBtn.addEventListener('click', ((e)=>{
-        e.preventDefault();
-        //console.log(allTasks);
-        localStorage.setItem('allTasks', JSON.stringify(allTasks));
-    }));
-}
-let emptyBtnHandler=(emptyBtn, allTasks)=>{
-    emptyBtn.addEventListener('click', function(e){
-        e.preventDefault();
-        allTasks=[];
-        //console.log('allTasks : line 175 ', allTasks);
-        localStorage.removeItem('allTasks');
-        location.reload(); 
-        return allTasks;       
-    })
-}
-let createFormHandler=(createForm, table, allTasks)=>{
-    table=createForm.addEventListener("submit", function(e){
-        e.preventDefault();
-        
-        let taskObj={
-            id: new Date().getTime(),
-            taskName:e.target.taskName.value,
-            status:'in progress',
-            position:'live'
-        }
-        e.target.taskName.value="";
-        allTasks.push(taskObj);
-        //console.log(allTasks);
-        let tr=$CE('TR');
-        tr=appendDataInRow(tr, taskObj, allTasks.length-1);
-       // console.log(tr);
-        let tbl=$GID('todoTable');
-        //console.log(tbl);
-        tbl=$APC(tbl, tr);
-        return tbl;
-    });
-}
-/*-------------------To Do Table Related functions----------------------------------------*/
-let changeStatus=(taskId)=>{
-    console.log(taskId);
-    let tr=document.getElementById(taskId);
-    console.log(tr.childNodes);
-    let child_nodes=tr.childNodes;
-    for(let i=0; i<child_nodes.length; i++){
-        if(child_nodes[i].firstChild.tagName==='INPUT'){
-            let x=child_nodes[i].firstChild;
-            if(x.classList.contains('status-btn')){
-                x.value="Complete";
-                x.style.backgroundColor="#080";
-                tr.style.backgroundColor='#ddd';
-            }
-        }
-        
-        
-        
-    }
-}
-
-let deleteRow=(taskId)=>{   
-    console.log(taskId);
-    let tr=$GID(taskId);
-    console.log(tr.parentNode);
-    tr.parentNode.removeChild(tr);
-    console.log(tr);
-    console.log(tr.parentNode);
-}
-let appendDataInRow=(tr, task, index)=>{
-    //console.log(tr);
-    tr.setAttribute("id", task.id);
-         let td1=$CE('td');
-         td1.innerHTML=index+1;
-         let td2=$CE('td');
-         td2.innerHTML=task.taskName;
-         let td3=$CE('td');        
-         td3.innerHTML= `<input type="button" 
-                                class="status-btn"
-                                value = "In Progress" 
-                                onClick="Javacsript:changeStatus(${task.id})">`;
-         let td4=$CE('td');
-         td4.innerHTML= `<input type="button"
-                                class="delete-btn" 
-                                value = "Delete" 
-                                onClick="Javacsript:deleteRow(${task.id})">`;
-         tr=$APCN(tr, td1, td2, td3, td4); 
-         //console.log(tr);       
-         return tr;
+/*---------------------------------------------*/
+let allTask=[];
+let displaySection=$GID('display-section');
+displaySection.innerHTML="";
+addHeader();
+addButtonBar();
+let table=createTable();
+allTasks=fetchAllTasks();
+if(allTasks.length > 0 ){
+    table=appendTableData(table, allTasks);
  }
-/* function to append table body */
-let appendTableData=(table, todolist)=>{
-    let tbody=$CE('tbody');
+ $APC(displaySection, table);
 
-    todolist.forEach((task, index) =>{        
-        let tr=$CE('tr');
-        tr=appendDataInRow(tr, task, index);
-        //console.log(tr);
-        tbody.appendChild(tr);    
-    });
-    table.appendChild(tbody);
-    return table;
-}
-/* function to fetch data from local storage */
-let fetchDataFromLocalStorage=()=>{
-    let allTasks = JSON.parse(localStorage.getItem('allTasks'));
-    //console.log(allTasks);
-    return allTasks;
-}
-/* function to append table head */
-let appendTableHead=(table, ...headings)=>{
-    //console.log(...headings);
-    let thead=$CE('thead');
-    let tr=$CE('tr');
-    let headingsArr= [...headings];
-    for(let headingName of headingsArr){
-        let th=$CE('TH');
-        th.innerText=headingName;
-        $APC(tr, th);
-    }
-    $APC(thead, tr);
-    $APC(table, thead);    
-    
-    return table;
-}
-let createTable=()=>{
-    let table=$CE('table');
-    table.setAttribute('id', 'todoTable');
-    table=appendTableHead(table, "Sr.", "Task Name",  "Mark Complete", "Delete");
-    return table;
+
+
+createFormHandler();
+saveBtnHandler();
+emptyBtnHandler();
+
+/* adding header to display section */
+function addHeader(){    
+    let header=$CE('div');
+    let h1=$CE('h1');
+    h1.innerText="To Do List";
+    $APC(header, h1);
+    $APC(displaySection, header);
 }
 /* adding buttons to display section */
-let addButtonBar=(displaySection)=>{
+function addButtonBar(){
     let div=$CE('div');
     div.setAttribute('class', 'buttonBar');
 
@@ -166,42 +59,168 @@ let addButtonBar=(displaySection)=>{
     $APC(div, emptyBtn);
     $APC(div, saveBtn);
     $APC(displaySection, div);
-    return displaySection;
 }
-/* adding header to display section */
-let addHeader=(displaySection)=>{    
-    let header=$CE('div');
-    let h1=$CE('h1');
-    h1.innerText="To Do List";
-    $APC(header, h1);
-    $APC(displaySection, header);
-    return displaySection;
+/* function to create table and its headings */
+function createTable(){
+    let table=$CE('table');
+    table.setAttribute('id', 'todoTable');
+    table=appendTableHead(table, "Sr.", "Task Name",  "Mark Complete", "Delete");
+    return table;
 }
-let fetchAllTasks=()=>{
-    let arr=fetchDataFromLocalStorage();
+/* function to append table head */
+function appendTableHead(table, ...headings){
+    //console.log(...headings);
+    let thead=$CE('thead');
+    let tr=$CE('tr');
+    let headingsArr= [...headings];
+    for(let headingName of headingsArr){
+        let th=$CE('TH');
+        th.innerText=headingName;
+        $APC(tr, th);
+    }
+    let tbody=$CE('TBODY');
+
+    $APC(thead, tr);
+    $APC(table, thead); 
+    $APC(table, tbody);   
+    
+    return table;
+}
+/* function to fetch all tasks from local storage or return null */
+function fetchAllTasks(){
+    let arr=JSON.parse(localStorage.getItem('allTasks'));
     arr = arr === null ? arr=[] : arr;   
    return arr;
 }
 
-let init=()=>{
-    let displaySection=$GID('display-section');
-    let table=createTable();
-    displaySection=addHeader(displaySection);
-    displaySection=addButtonBar(displaySection);
-     
-    $APC(displaySection, table);
-    let allTasks=fetchAllTasks();
-    //console.log(allTasks);
-    appendTableData(table, allTasks);
-    //table=createNewTask(table);
+/*function to append data of allTasks into Table */
+function appendTableData(table){
+    console.log(allTasks);
+    allTasks.forEach((task, index) =>{ 
+        console.log(allTasks);       
+        let tr=$CE('tr');
+        tr=appendDataInRow(tr, task, index, allTasks);
+        //console.log(tr);
+        table.appendChild(tr);    
+    });
     
-    let createForm = $GID("create-form");
-    createFormHandler(createForm, table, allTasks);
-    let saveBtn=$GID('saveBtn');
-    saveBtnHandler(saveBtn, allTasks);
-    let emptyBtn=$GID('emptyBtn');
-    emptyBtnHandler(emptyBtn, allTasks);
+    return table;
 }
 
-window.onload=init;
+function appendDataInRow(tr, task, index){
+    console.log(task);
+    tr.setAttribute("id", task.id);
+         let td1=$CE('td');
+         td1.innerHTML=index+1;
+         let td2=$CE('td');
+         td2.innerHTML=task.taskName;
+         let td3=$CE('td');        
+         td3.innerHTML= `<input type="button" 
+                                class="status-btn"
+                                value = "${task.status}" 
+                                onClick="Javacsript:changeStatus(${task.id})">`;
+         let td4=$CE('td');
+         td4.innerHTML= `<input type="button"
+                                class="delete-btn" 
+                                value = "Delete" 
+                                onClick="Javacsript:deleteRow(${task.id})">`;
+         tr=$APCN(tr, td1, td2, td3, td4); 
+         //console.log(tr);       
+         return tr;
+ }
+/*-------------------------Event Handlers--------------------------------*/
+function createFormHandler(){
+    let createForm = $GID("create-form");
+    createForm.addEventListener("submit", function(e){
+        e.preventDefault();
+        console.log(e.target.taskName.value);
+        let taskObj={
+            id: new Date().getTime(),
+            taskName:e.target.taskName.value,
+            status:'in progress',
+            position:'current'
+        }
+        e.target.taskName.value="";
+        allTasks.push(taskObj);
+        let tr=$CE('TR');
+        tr=appendDataInRow(tr, taskObj, allTasks.length-1);
+        
+        $APC(table, tr);        
+        console.log(allTasks);        
+    });
+    console.log(allTasks);
+}
+
+function saveData(todolist){
+    console.log(todolist);
+    localStorage.setItem('allTasks', JSON.stringify(todolist));
+    //location.reload();
+}
+function saveBtnHandler(){
+    let saveBtn=$GID('saveBtn');
+    saveBtn.addEventListener('click', ((e)=>{
+        e.preventDefault();
+        console.log(allTasks);
+        localStorage.setItem('allTasks', JSON.stringify(allTasks));
+    }));
+}
+function emptyBtnHandler(){
+    let emptyBtn=$GID('emptyBtn')
+    emptyBtn.addEventListener('click', function(e){
+        console.log("empty button clicked");
+        e.preventDefault();
+        allTasks=[];
+        console.log('allTasks : line 175 ', allTasks);
+        localStorage.removeItem('allTasks');
+        location.reload();       
+    })
+}
+
+let changeStatus=(taskId)=>{
+    console.log(taskId);    
+    let tr=document.getElementById(taskId);
+    console.log(tr.childNodes);
+    let child_nodes=tr.childNodes;
+    for(let i=0; i<child_nodes.length; i++){
+        if(child_nodes[i].firstChild.tagName==='INPUT'){
+            let x=child_nodes[i].firstChild;
+            if(x.classList.contains('status-btn')){
+                x.value="Complete";
+                x.setAttribute('class', 'complete-btn')
+                tr.setAttribute('class', 'complete-row');
+                updateAllTasks(taskId, 'changeStatus');              
+            }
+        }     
+    }
+}
+
+let deleteRow=(taskId)=>{   
+    console.log(taskId);
+    let tr=$GID(taskId);
+    console.log(tr.parentNode);
+    tr.parentNode.removeChild(tr);
+    console.log(tr);
+    console.log(tr.parentNode);
+    updateAllTasks(taskId, 'deleteItem');
+}
+
+function updateAllTasks(id, searchItem){
+    if(searchItem=='changeStatus'){
+        allTasks.forEach((item)=>{
+            if(item.id==id){
+                item.status="Complete";
+                saveData(allTasks);
+            }
+        })
+    } else if(searchItem=='deleteItem'){
+        allTasks.forEach((item, index)=>{
+            if(item.id==id){
+                allTasks.splice(index, 1);
+            }
+        })
+    }
+    console.log(allTasks);
+}
+
+
 
