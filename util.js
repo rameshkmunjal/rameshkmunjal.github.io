8a80f43caefcfa2841f6c8f084fa1dba06eb516f
ramesh kumar munjal <rameshkmunjal.com>
2021-11-15 22:44:43 +0530

    portfolio page change

--- util.js
+++ util.js
@@ -1,3 +1,16 @@
+/*
+ * utility functions - some DOM related methods
+ * can be used in all js files
+*/
 
-let cre_txt = (txt) => document.createTextNode(txt);
+//grab an element by ID
+export const $GID=(ele)=>document.getElementById(ele);
 
+//create element
+export const $CE=(ele)=> document.createElement(ele);
+
+//append child to parent
+export const $APC=(parent , child)=>parent.appendChild(child);
+
+//set class of a givent element
+export const $SET_CLASS=(ele, className)=> ele.setAttribute('class', className);
