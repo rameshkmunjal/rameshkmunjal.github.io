7ab1dde9ee1d0bce6c33c605af148a42e7210b6d
ramesh kumar munjal <rameshkmunjal.com>
2022-01-10 17:28:10 +0530

    e-shop react project included

--- CONSTANTS.js
+++ CONSTANTS.js
@@ -443,4 +443,14 @@ export const reactData=[
         "url": "https://rameshkmunjal.github.io/movies/",
         "tech":"React"
     },
+    ,
+    {
+        "id": "3",
+        "category": "reactjs",
+        "name": "e-shop",
+        "created_on":"2022-01-10",
+        "imgsrc":'https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg',
+        "url": "https://rameshkmunjal.github.io/e-shop/",
+        "tech":"React"
+    },
 ];
