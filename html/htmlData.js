39dba00377a595c0bf68d72d78d217065b625fc5
ramesh kumar munjal <rameshkmunjal.com>
2022-05-19 12:40:01 +0530

    html data changed

--- html/htmlData.js
+++ html/htmlData.js
@@ -13,99 +13,21 @@ const htmlData = [
         "category": "html",
         "name": "parallaxBG-2",
         "created_on":"2022-04-07",
-        "imgsrc":"../images/angular.png",
+        "imgsrc":"../images/html.png",
         "url": "../projects/htmlProjects/parallaxBG-2/index.html",
         "tech":"html-css"
     },
-    {
-        "id": "1",
-        "category": "html",
-        "name": "business-website",
-        "created_on":"2022-04-01",
-        "imgsrc":"../images/html.png",
-        "url": "../projects/htmlProjects/business_ws_page/index.html",
-        "tech":"html-css"
-    },
-    {
-        "id": "2",
-        "category": "html",
-        "name": "parallaxBG-2",
-        "created_on":"2022-04-07",
-        "imgsrc":"../images/angular.png",
-        "url": "../projects/jsProjects/parallaxBG-2/index.html",
-        "tech":"html-css"
-    },
+    
+    
     {
         "id": "3",
         "category": "html",
-        "name": "business-website",
+        "name": "testimonials grid",
         "created_on":"2022-04-01",
-        "imgsrc":"../images/html.png",
+        "imgsrc":"../images/grid.png",
         "url": "../projects/htmlProjects/testimonials-grid/index.html",
         "tech":"html-css"
     },
-    {
-        "id": "2",
-        "category": "html",
-        "name": "parallaxBG-2",
-        "created_on":"2022-04-07",
-        "imgsrc":"../images/angular.png",
-        "url": "../projects/jsProjects/parallaxBG-2/index.html",
-        "tech":"html-css"
-    },
-    {
-        "id": "1",
-        "category": "html",
-        "name": "business-website",
-        "created_on":"2022-04-01",
-        "imgsrc":"../images/html.png",
-        "url": "../projects/htmlProjects/business_ws_page/index.html",
-        "tech":"html-css"
-    },
-    {
-        "id": "2",
-        "category": "html",
-        "name": "parallaxBG-2",
-        "created_on":"2022-04-07",
-        "imgsrc":"../images/angular.png",
-        "url": "../projects/jsProjects/parallaxBG-2/index.html",
-        "tech":"html-css"
-    },
-    {
-        "id": "1",
-        "category": "html",
-        "name": "business-website",
-        "created_on":"2022-04-01",
-        "imgsrc":"../images/html.png",
-        "url": "../projects/htmlProjects/business_ws_page/index.html",
-        "tech":"html-css"
-    },
-    {
-        "id": "2",
-        "category": "html",
-        "name": "parallaxBG-2",
-        "created_on":"2022-04-07",
-        "imgsrc":"../images/angular.png",
-        "url": "../projects/jsProjects/parallaxBG-2/index.html",
-        "tech":"html-css"
-    },
-    {
-        "id": "1",
-        "category": "html",
-        "name": "business-website",
-        "created_on":"2022-04-01",
-        "imgsrc":"../images/html.png",
-        "url": "../projects/htmlProjects/business_ws_page/index.html",
-        "tech":"html-css"
-    },
-    {
-        "id": "2",
-        "category": "html",
-        "name": "parallaxBG-2",
-        "created_on":"2022-04-07",
-        "imgsrc":"../images/angular.png",
-        "url": "../projects/jsProjects/parallaxBG-2/index.html",
-        "tech":"html-css"
-    },
+    
       
 ];
